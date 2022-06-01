8b5889c66eeb7ea1c91d65bb6b18be265f052ce8
Nico Calvelo <dia.calvelo.nicolas@gmail.com>
2022-06-01 14:54:19 -0300

    ep 09

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Entity\Category;
 use App\Form\Model\BookDto;
+use App\Form\Model\CategoryDto;
 use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
+use App\Repository\CategoryRepository;
 use App\Service\FileUploader;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -14,13 +18,15 @@ use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
-class BooksController extends AbstractFOSRestController{
+class BooksController extends AbstractFOSRestController
+{
 
     /**
      * @Rest\Get(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function getAction(BookRepository $bookRepository){
+    public function getAction(BookRepository $bookRepository)
+    {
         return $bookRepository->findAll();
     }
 
@@ -28,25 +34,83 @@ class BooksController extends AbstractFOSRestController{
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function postAction(EntityManagerInterface $em, Request $request, FileUploader $fileUploader){
+    public function postAction(EntityManagerInterface $em, Request $request, FileUploader $fileUploader)
+    {
         $bookDto = new BookDto();
         $form = $this->createForm(BookFormType::class, $bookDto);
         $form->handleRequest($request);
-        if($form->isSubmitted()){
+        if ($form->isSubmitted()) {
             return new Response('', HttpFoundationResponse::HTTP_BAD_REQUEST);
         }
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $book = new Book();
             $book->setTitle($bookDto->title);
-            if($bookDto->base64Image){
+            if ($bookDto->base64Image) {
                 $filename = $fileUploader->uploadBase64File($bookDto->base64Image);
                 $book->setImage($filename);
             }
             $em->persist($book);
             $em->flush();
-            return($book);
+            return ($book);
         }
         return $form;
     }
-    
+
+    /**
+     * @Rest\Post(path="/books/{id}", requirements={"id"="\d+"})
+     * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function editAction(int $id, EntityManagerInterface $em, BookRepository $bookRepository, CategoryRepository $categoryRepository, Request $request, FileUploader $fileUploader)
+    {
+        $book = $bookRepository->find($id);
+        if (!$book) {
+            throw $this->createNotFoundException('Book not found');
+        }
+        $bookDto = BookDto::createFromBook($book);
+
+        $originalCategories = new ArrayCollection();
+        foreach ($book->getCategories() as $category) {
+            $categoryDto = CategoryDto::createFromCategory($category);
+            $bookDto->categories[] = $categoryDto;
+            $originalCategories->add($categoryDto);
+        }
+        $form = $this->createForm(BookFormType::class, $bookDto);
+        $form->handleRequest($request);
+        if (!$form->isSubmitted()) {
+            return new Response('', HttpFoundationResponse::HTTP_BAD_REQUEST);
+        }
+        if ($form->isValid()) {
+            // Remove categories
+            foreach ($originalCategories as $originalCategoryDto) {
+                if (!in_array($originalCategoryDto, $bookDto->categories)) {
+                    $category = $categoryRepository->find(($originalCategoryDto->id));
+                    $book->removeCategory($category);
+                }
+            }
+
+            // Add categories
+            foreach ($bookDto->categories as $newCategoryDto) {
+                if (!$originalCategories->contains($newCategoryDto)) {
+                    $category = $categoryRepository->find($newCategoryDto->id ?? 0);
+                    if (!$category) {
+                        $category = new Category();
+                        $category->setName($newCategoryDto->name);
+                        $em->persist($category);
+                    }
+                    $book->addCategory($category);
+                }
+            }
+
+            $book->setTitle($bookDto->title);
+            if ($bookDto->base64Image) {
+                $filename = $fileUploader->uploadBase64File($bookDto->base64Image);
+                $book->setImage($filename);
+            }
+            $em->persist($book);
+            $em->flush();
+            $em->refresh($book);
+            return $book;
+        }
+        return $form;
+    }
 }
